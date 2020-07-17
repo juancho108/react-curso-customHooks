8fb6731d85c3036b4ffaa078f12c284d42c329fb
juancho108 <jjf108@gmail.com>
2020-07-17 20:41:58 -0300

    primer commit

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Message } from "./Message";
+import "./effects.css";
+
+export const SimpleForm = () => {
+  const [formState, setformState] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = formState;
+
+  useEffect(() => {
+    // console.log("hey");
+  }, []);
+
+  useEffect(() => {
+    // console.log("formState  cambió! ");
+  }, [formState]);
+
+  useEffect(() => {
+    // console.log("email  cambió! ");
+  }, [email]);
+
+  const handleInputChange = ({ target }) => {
+    setformState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+
+      <div className="form-group">
+        <input
+          type="name"
+          name="name"
+          className="form-control"
+          placeholder="Tu nombre"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="name"
+          name="email"
+          className="form-control"
+          placeholder="email@gmail.com"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      {name === "123" && <Message />}
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
+// import { FocusScreen } from "./components/04-useRef/FocusScreen";
+// import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
+// import { FormWithCustomHook } from "./components/02-useEffect/FormWithCustomHook";
+// import { HookApp } from "./HookApp";
+// import { CounterApp } from "./components/01-useState/counterApp";
+// import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+// import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<RealExampleRef />, document.getElementById("root"));
